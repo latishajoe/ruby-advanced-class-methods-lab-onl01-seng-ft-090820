f3e5d9027841c25c3da00f20a31d5edf35472b4c
Latisha Joe <latisha_joe@yahoo.com>
2020-09-21 23:01:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,12 +49,12 @@ class Song
   end
 
   def self.create_from_filename(name)
-    song = self.new 
+    song = self.new
     song.name = (name.split(" - ") [1].chomp(".mp3"))
     song.artist_name = (name.split(" - ") [0])
-    @@all << song 
-    song 
-  end 
+    @@all << song
+    song
+  end
 
 
 
